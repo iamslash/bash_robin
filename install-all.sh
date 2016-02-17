2c378af5c16a698775c5b426d2c431d08b17e32a
iamslash <iamslash@ncrewui-MacBook-Pro.local>
2016-02-17 22:02:27 +0900

    added install, uninstall script

---
+++ install-all.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+BASH_IT_HOME=~/.bash_it
+
+warn() {
+    echo "$1" >&2
+}
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+# validate
+[ -e "$BASH_IT_HOME" ] && die "$BASH_IT_HOME already exists."
+
+# clone bash-it
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+
+echo "bash-it is installed."
---
+++ uninstall-all.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+BASH_IT_HOME=~/.bash_it
+
+rm -rf "$BASH_IT_HOME"
+
+echo "bash-it is uninstalled."
